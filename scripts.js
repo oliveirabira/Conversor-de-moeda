94ff39eb1da614e8843145e5d82346d5418dad6d
oliveirabira <ubiratan001@hotmail.com>
2023-08-25 13:02:28 -0300

    adicionada novas moedas e imagens

--- scripts.js
+++ scripts.js
@@ -13,6 +13,13 @@ function convertValues() {
     const euroToday = 5.35
     const libraToday = 6.29
     const bitcoinToday = 127.847
+    const bahtToday = 0.14
+    const liraToday = 0.19
+    const rubloToday = 0.051
+    const ruppiaToday = 0.059
+    const wonToday = 0.0037
+    const ieneToday = 0.0033
+    const yuanToday = 0.67
 
     if (currencySelect.value == "dolar") {  //Se o select estiver selecionado o valor de dolar entre aqui
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -38,6 +45,48 @@ function convertValues() {
             currency: "BTC"
         }).format(inputCurrencyValue / bitcoinToday)
     }
+    if (currencySelect.value == "baht") {  //Se o select estiver selecionado o valor de bath entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("th-TH", {
+            style: "currency",
+            currency: "THB"
+        }).format(inputCurrencyValue / bahtToday)
+    }
+    if (currencySelect.value == "lira-turca") {  //Se o select estiver selecionado o valor de lira turca entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("tr-TR", {
+            style: "currency",
+            currency: "TRY"
+        }).format(inputCurrencyValue / liraToday)
+    }
+    if (currencySelect.value == "rublo-russo") {  //Se o select estiver selecionado o valor de rublo russo entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("ru-RU", {
+            style: "currency",
+            currency: "RUB"
+        }).format(inputCurrencyValue / rubloToday)
+    }
+    if (currencySelect.value == "rupia-indiana") {  //Se o select estiver selecionado o valor de rupia indiana entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("hi-IN", {
+            style: "currency",
+            currency: "INR"
+        }).format(inputCurrencyValue / ruppiaToday)
+    }
+    if (currencySelect.value == "won") {  //Se o select estiver selecionado o valor de won entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("ko-KR", {
+            style: "currency",
+            currency: "KRW"
+        }).format(inputCurrencyValue / wonToday)
+    }
+    if (currencySelect.value == "iene") {  //Se o select estiver selecionado o valor de iene entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("ja-JP", {
+            style: "currency",
+            currency: "JPY"
+        }).format(inputCurrencyValue / ieneToday)
+    }
+    if (currencySelect.value == "yuan") {  //Se o select estiver selecionado o valor de yuan entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("zn-CN", {
+            style: "currency",
+            currency: "CNY"
+        }).format(inputCurrencyValue / yuanToday)
+    }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -47,7 +96,7 @@ function convertValues() {
 
 }
 
-function changeCurrency () {
+function changeCurrency() {
     const currencyName = document.getElementById("currency-name")
     const currencyImage = document.querySelector(".currency-img")
 
@@ -65,15 +114,50 @@ function changeCurrency () {
         currencyName.innerHTML = "Libra"
         currencyImage.src = "./assets/libra.png"
     }
-    
+
     if (currencySelect.value == "bitcoin") {
         currencyName.innerHTML = "Bitcoin"
         currencyImage.src = "./assets/bitcoin.png"
     }
 
+    if (currencySelect.value == "baht") {
+        currencyName.innerHTML = "Baht"
+        currencyImage.src = "./assets/baht.png"
+    }
+
+    if (currencySelect.value == "lira-turca") {
+        currencyName.innerHTML = " Lira Turca"
+        currencyImage.src = "./assets/lira-turca.png"
+    }
+
+    if (currencySelect.value == "rublo-russo") {
+        currencyName.innerHTML = "Rublo Russo"
+        currencyImage.src = "./assets/rublo.png"
+    }
+
+    if (currencySelect.value == "rupia-indiana") {
+        currencyName.innerHTML = "Rupia Indiana"
+        currencyImage.src = "./assets/rupia.png"
+    }
+
+    if (currencySelect.value == "won") {
+        currencyName.innerHTML = "Won"
+        currencyImage.src = "./assets/won.png"
+    }
+
+    if (currencySelect.value == "iene") {
+        currencyName.innerHTML = "Iene"
+        currencyImage.src = "./assets/iene.png"
+    }
+
+    if (currencySelect.value == "yuan") {
+        currencyName.innerHTML = "Yuan"
+        currencyImage.src = "./assets/yuan.png"
+    }
+
     convertValues()
-    
+
 }
 
-currencySelect.addEventListener("change",changeCurrency)  //Ouvinte no select quando muda o valor
+currencySelect.addEventListener("change", changeCurrency)  //Ouvinte no select quando muda o valor
 convertButton.addEventListener("click", convertValues) //Ouvinte no click quando muda o valor
